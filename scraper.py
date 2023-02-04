9ae2ca405ad657a26e072d49ea7d2e478cf0ef03
Jaime Chirawan Markkern <jmarkker@circinus-48.ics.uci.edu>
2023-02-04 14:16:43 -0800

    added crawler trap detection and started robots.txt read

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import re
 from urllib.parse import urlparse
+from urllib.parse import urljoin
+from urllib.parse import urldefrag
 from bs4 import BeautifulSoup
+import urllib.robotparser as rp
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -17,21 +20,39 @@ def extract_next_links(url, resp):
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     
-    if resp.status != 200:
-        return []
+    hyperlink_list = []                                                     # initialize empty list
+    hyperlink_set = set()                                                   # initialize empty set (to remove duplicates)
+    if resp.status == 200:
+        # print("Status code: {}, success.".format(resp.status))   # PRINT CHECK
+        page_soup = BeautifulSoup(resp.raw_response.content, "html.parser") # create the BeautifulSoup object
+        
+        previous_absolute = resp.url                                        # first url should be absolute since it's in frontier
+        for link in page_soup.find_all("a"):                                # list of anchor elements in HTML
+            hyperlink = link.get('href')                                    # check if element has href attribute
+            if (not is_absolute(hyperlink)):
+                hyperlink = urljoin(previous_absolute, hyperlink)
+            else:
+                previous_absolute = hyperlink
 
-    try:
-        hyperlinks = []
-        soup = BeautifulSoup(resp.raw_response.content, "html.parser")
-        links = soup.find_all("a")
-        for link in links:
-            l_url = link["href"]
-            if is_valid(l_url):
-                hyperlinks.append(l_url)
-        return hyperlinks
+            if (urldefrag(hyperlink)[0] != ""):
+                hyperlink = urldefrag(hyperlink)[0]
+            
+            hyperlink_set.add(hyperlink)
 
-    except:
-        return []
+    else:
+        print("Status code: {}, error, {}.".format(resp.status,url))        # PRINT CHECK
+    
+    hyperlink_list = list(hyperlink_set)                                    # convert set of links into list
+
+    return hyperlink_list
+
+def is_absolute(url):
+    # determines if a url is absolute by checking if it has a scheme and a domain
+    parsed = urlparse(url)
+    if parsed.scheme != "" and parsed.hostname != "":
+        return True
+    else:
+        return False 
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -41,11 +62,17 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        # check if in domain: not in domain -> false
-        # print(parsed.hostname)
-        if not (parsed.hostname.find("ics.uci.edu") > -1 or parsed.hostname.find("cs.uci.edu") > -1
-            or parsed.hostname.find("informatics.uci.edu") > -1 or parsed.hostname.find("stat.uci.edu") > -1):
+
+        # check if url is within domain
+        if not re.match(r"^.*\.(ics\.uci\.edu|cs\.uci\.edu|informatics\.uci\.edu|stat\.uci\.edu)\/.*$", url):
             return False
+        
+        # crawler trap detection
+        if re.match(r"^.*?(\/.+?\/).*?\1.*$|^.*?\/(.+?\/)\2.*$"                 # repeating directories https://support.archive-it.org/hc/en-us/articles/208332943-Identify-and-avoid-crawler-traps-
+                    + r"^.*?\?.+\=.+(\&.+\=.+)+?.*$"                            # long queries ?query=abc&abc=abc&abc=abc...scr
+                    + r"wics\.ics\.edu\/.*(week|d{4}\-\d{2}\-\d{2}).*$", url):  # wics calendar
+            return False
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -57,5 +84,11 @@ def is_valid(url):
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
     except TypeError:
-        print ("TypeError for ", parsed)
+        print ("TypeError for ", url, parsed)
         raise
+
+def robot_test(url):
+    rparser = rp.RobotFileParser()
+    rparser.set_url(url + "/robots.txt")
+    rparser.read()
+    return rparser.can_fetch("*", url)
